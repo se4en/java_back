78833c9afe24cc1d08a5539db6bf5ec73f6ce265
se4en <001122mj@gmail.com>
2021-03-25 23:54:21 +0300

    Add Project

--- src/main/java/entities/Project.java
+++ src/main/java/entities/Project.java
@@ -1,4 +1,95 @@
 package entities;
 
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table (name = "projects")
 public class Project {
+
+    @Column(name = "proj_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+    private String description;
+    private java.sql.Timestamp start_date;
+    private java.sql.Timestamp end_date;
+    private String status;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Timestamp start_date) {
+        this.start_date = start_date;
+    }
+
+    public Timestamp getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Timestamp end_date) {
+        this.end_date = end_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Project project = (Project) o;
+        return id == project.id &&
+                Objects.equals(title, project.title) &&
+                Objects.equals(description, project.description) &&
+                Objects.equals(start_date, project.start_date) &&
+                Objects.equals(end_date, project.end_date) &&
+                Objects.equals(status, project.status);
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", start_date=" + start_date +
+                ", end_date=" + end_date +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
