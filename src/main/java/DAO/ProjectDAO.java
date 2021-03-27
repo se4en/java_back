830e8b33d253a2fa9336496395ea76923f9fe605
se4en <001122mj@gmail.com>
2021-03-27 11:06:28 +0300

    Add project DAO

---
+++ src/main/java/DAO/ProjectDAO.java
@@ -0,0 +1,49 @@
+package DAO;
+
+import entities.Project;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import utils.HibernateSessionFactoryUtil;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
+
+
+public class ProjectDAO {
+
+    public Project findById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(Project.class, id);
+    }
+
+    public void save(Project project) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.save(project);
+        tx1.commit();
+        session.close();
+    }
+
+    public void update(Project project) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.update(project);
+        tx1.commit();
+        session.close();
+    }
+
+    public void delete(Project project) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.delete(project);
+        tx1.commit();
+        session.close();
+    }
+
+    public List<Project> findAll() {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        CriteriaQuery<Project> criteria = session.getCriteriaBuilder().createQuery(Project.class);
+        criteria.from(Project.class);
+        List<Project> projects = session.createQuery(criteria).getResultList();
+        session.close();
+        return projects;
+    }
+}
