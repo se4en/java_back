82a0aa9702f6e2e7f9bc199aa153de67468acd1c
se4en <001122mj@gmail.com>
2021-05-10 14:00:35 +0300

    Add glassfish

--- src/main/java/controllers/App.java
+++ src/main/java/controllers/App.java
@@ -2,10 +2,21 @@ package controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class App {
+    private static ApplicationContext applicationContext;
+
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        applicationContext = SpringApplication.run(App.class, args);
+        displayAllBeans();
+    }
+
+    public static void displayAllBeans() {
+        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        for(String beanName : allBeanNames) {
+            System.out.println(beanName);
+        }
     }
 }
